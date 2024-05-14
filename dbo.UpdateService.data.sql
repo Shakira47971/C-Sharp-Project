379e0d8a8d90fae34663c63e6055ed504213d7f2
Shakira47971 <159004578+Shakira47971@users.noreply.github.com>
2024-05-14 21:02:50 +0600

    Add files via upload

---
+++ dbo.UpdateService.data.sql
@@ -0,0 +1,7 @@
+﻿INSERT INTO [dbo].[UpdateService] ([Code], [Catagory], [Name], [Price]) VALUES (N'', N'', N'', N'')
+INSERT INTO [dbo].[UpdateService] ([Code], [Catagory], [Name], [Price]) VALUES (N'112', N'Manicure', N'Gel Manicure', N'789')
+INSERT INTO [dbo].[UpdateService] ([Code], [Catagory], [Name], [Price]) VALUES (N'113', N'Facial', N'Anti Tan Facial', N'1860')
+INSERT INTO [dbo].[UpdateService] ([Code], [Catagory], [Name], [Price]) VALUES (N'114', N' Facial', N'Hydra Facial', N'1140')
+INSERT INTO [dbo].[UpdateService] ([Code], [Catagory], [Name], [Price]) VALUES (N'116', N'Hair Cut', N'Layer Cut', N'990')
+INSERT INTO [dbo].[UpdateService] ([Code], [Catagory], [Name], [Price]) VALUES (N'117', N'Hair Cut', N'V Cut', N'670')
+INSERT INTO [dbo].[UpdateService] ([Code], [Catagory], [Name], [Price]) VALUES (N'121', N'Make Up', N'Party Look', N'3450')
